4ce08e838ecad1f75af318365fe68b5cfd49245d
chanaKastner <chana64065@gmail.com>
2024-06-25 13:09:59 +0300

    update

--- my-react-app/src/components/routes.jsx
+++ my-react-app/src/components/routes.jsx
@@ -3,9 +3,7 @@ import { Home } from "./Home"
 import { About } from "./About"
 import { Flowers } from "./flowers"
 import { Contact } from "./צור קשר"
-import { FlowersTry } from "../style/flowertry"
 import { Sub_Categories } from "./sub_categories"
-// import { ImageGallery } from "./aaaa"
 
 export const NavRoutes = () => {
     return <>
@@ -16,8 +14,7 @@ export const NavRoutes = () => {
             <Route path="flowers/:name" element={<Flowers />}></Route>
             <Route path="categories/:category" element={<Sub_Categories />}></Route>
             <Route path="contact" element={<Contact />}></Route>
-            <Route path="try" element={<FlowersTry />}></Route>
-            {/* <Route path="aaaa" element={<ImageGallery />}></Route> */}
+
         </Routes>
     </>
 }
